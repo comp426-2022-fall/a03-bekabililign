b9d657aadcd4d048c8e588d80bd72b22d4bb8f4c
Beka Bililign <bekab@email.unc.edu>
2022-11-09 18:22:23 -0500

    completed a03

---
+++ bin/cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import { roll } from "../lib/roll.js";
+
+
+const args = minimist(process.argv.slice(2))
+// console.log(args)
+
+if (args.sides) {
+    var sides = args.sides;
+}
+else {
+    var sides = 6;
+}
+
+if (args.dice) {
+    var dice = args.dice;
+}
+else {
+    var dice = 2;
+}
+
+if (args.rolls) {
+    var rolls = args.rolls;
+}
+else {
+    var rolls = 1;
+}
+
+console.log(JSON.stringify(roll(sides, dice, rolls)))
